a198f171cde01062b6ebb206f1ecdb8b8e345de7
Manohar Mukku <manoharmukku@gmail.com>
2018-09-26 16:10:08 +0530

    Added separate function for getting arguments

--- mlp.py
+++ mlp.py
@@ -9,10 +9,10 @@ import sys
 import getopt
 import pandas as pd
 
-def main(argv):
+def get_arguments(argv):
     '''
-    Main function which controls the flow of the program
-    Arguments: argv - command line arguments and flags
+    Get command line arguments using getopt
+    Argument: argv which contains arguments and flags
     '''
 
     # Get the command line arguments
@@ -57,33 +57,33 @@ def main(argv):
     try:
         lr = float(lr)
     except ValueError:
-        sys.exit("Oops! Plese enter a float value for learning rate")
+        sys.exit("Oops! Learning rate should be a float value")
     if (lr <= 0):
-        sys.exit("Oops! Please enter a positive float value for momentum")
+        sys.exit("Oops! Leraning rate should be positive")
 
     # Sanity check momentum
     try:
         momentum = float(momentum)
     except ValueError:
-        sys.exit("Oops! Please enter a float value for momentum")
+        sys.exit("Oops! Momentum should be a float value")
     if (momentum < 0):
-        sys.exit("Oops! Please enter a non-negative float value for momentum")
+        sys.exit("Oops! Momentum should be non-negative")
 
     # Sanity check batch size
     try:
         batch_size = int(batch_size)
     except ValueError:
-        sys.exit("Oops! Please enter an integer value for batch size")
+        sys.exit("Oops! Batch size should be an integer value")
     if (batch_size <= 0):
-        sys.exit("Oops! Please enter a positive integer value for batch size")
+        sys.exit("Oops! Batch size should be positive")
 
     # Sanity check regularization
     try:
         regularization = float(regularization)
     except ValueError:
-        sys.exit("Oops! Please enter a float value for regularization parameter")
+        sys.exit("Oops! Regularization should be a float value")
     if (regularization < 0):
-        sys.exit("Oops! Please enter a non-negative float value for regularization parameter")
+        sys.exit("Oops! Regularization should be non-negative")
 
     # Sanity check file
     with open(file) as data_file:
@@ -93,15 +93,28 @@ def main(argv):
     try:
         seed = int(seed)
     except ValueError:
-        sys.exit("Oops! Enter an integer value for seed")
+        sys.exit("Oops! Random seed should be an integer value")
 
     # Sanity check for iterations
     try:
         max_iter = int(max_iter)
     except ValueError:
-        sys.exit("Oops! Please enter an integer value for iterations")
+        sys.exit("Oops! Number of iterations should be an integer value")
     if (max_iter <= 0):
-        sys.exit("Oops! Please enter a positive integer value for iterations")
+        sys.exit("Oops! Number of iterations should be positive")
+
+    return lr, momentum, batch_size, regularization, df, seed, max_iter
+
+def main(argv):
+    '''
+    Main function which controls the flow of the program
+    Arguments: argv - command line arguments and flags
+    '''
+
+    # Get the parameters from the command line
+    lr, momentum, batch_size, regularization, df, seed, max_iter = get_arguments(argv)
+
+
 
 
 if __name__ == "__main__":
